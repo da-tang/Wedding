eb3a664752e488971cc246f512dbc0dd42426790
Larry Tang <hankydream@hotmail.ca>
2019-05-13 02:29:26 -0400

    remove moogose db

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const historyApiFallback = require('connect-history-api-fallback');
-const mongoose = require('mongoose');
 const path = require('path');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
-const config = require('../config/config');
 const webpackConfig = require('../webpack.config');
 
 const isDev = process.env.NODE_ENV !== 'production';
@@ -16,10 +14,6 @@ const port  = process.env.PORT || 8080;
 
 // Configuration
 // ================================================================================================
-
-// Set up Mongoose
-mongoose.connect(isDev ? config.db_dev : config.db);
-mongoose.Promise = global.Promise;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
